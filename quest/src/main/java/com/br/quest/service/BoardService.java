11cd86db35ace3b97f2c3dba3aa35c2c354591f3
jose <jose.claercio@esig.com.br>
2025-07-04 00:14:58 -0300

    pgdata ignore

--- quest/src/main/java/com/br/quest/service/BoardService.java
+++ quest/src/main/java/com/br/quest/service/BoardService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import com.br.quest.model.Board;
 import com.br.quest.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BoardService {
 
     @Autowired
